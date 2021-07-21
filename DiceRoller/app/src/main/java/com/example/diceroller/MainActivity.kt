a9bdf1f955139f6a9bb625ffa5b15a4e4d0c206a
“tastethedream” <tastethedream@gmail.com>
2021-07-21 14:43:08 +0100

    when statment added to change dice images

--- DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,7 +3,8 @@ package com.example.diceroller
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
+import android.widget.ImageView
+
 
 /**
  * This activity allows the user to roll a dice and view the result
@@ -24,9 +25,11 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val rollButton: Button = findViewById(R.id.button)
-        rollButton.setOnClickListener {
-            rollDice()
-        }
+        rollButton.setOnClickListener { rollDice() }
+
+        // Do a dice roll when the app starts
+        rollDice()
+
     }
     /**
      * Roll the dice and update the screen with the result.
@@ -36,8 +39,21 @@ class MainActivity : AppCompatActivity() {
         val dice = Dice(6)
         val diceRoll = dice.roll()
         // Update the screen with the dice roll
-        val resultTextView: TextView = findViewById(R.id.textView)
-        resultTextView.text = diceRoll.toString()
+        // Find the ImageView in the layout
+       val diceImage: ImageView = findViewById(R.id.imageView)
+        // Determine which drawable resource ID to use based on the dice roll
+        val drawableResource = when (diceRoll) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            6 -> R.drawable.dice_6
+            else -> R.drawable.dice_6
+        }
 
+        diceImage.setImageResource(drawableResource)
+        //content desc for image view
+        diceImage.contentDescription = diceRoll.toString()
     }
 }
