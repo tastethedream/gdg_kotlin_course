543fea8db750899125f0f2e77bce4736b1590303
“tastethedream” <tastethedream@gmail.com>
2021-08-26 11:28:25 +0100

    detail activity added to allow navifgation to the selected letter

--- words_app/words_app/app/src/main/java/com/example/wordsapp/DetailActivity.kt
+++ words_app/words_app/app/src/main/java/com/example/wordsapp/DetailActivity.kt
@@ -24,6 +24,7 @@ import com.example.wordsapp.databinding.ActivityDetailBinding
 
 class DetailActivity : AppCompatActivity() {
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -32,11 +33,13 @@ class DetailActivity : AppCompatActivity() {
         // For example, a View with the id word_one is referenced as binding.wordOne
         val binding = ActivityDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
         // Retrieve the LETTER from the Intent extras
         // intent.extras.getString returns String? (String or null)
         // so toString() guarantees that the value will be a String
-        val letterId = "A"
+        val letterId = intent?.extras?.getString("letter").toString()
+
+    
+        //val letterId = "A"
 
         val recyclerView = binding.recyclerView
         recyclerView.layoutManager = LinearLayoutManager(this)
